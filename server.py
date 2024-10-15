1896e4512b0a4272fd71aa618ce6e217a0ac5b5b
Molnár "Scopsy" Maxim <72470504+szkobzi@users.noreply.github.com>
2024-10-15 14:03:41 +0200

    Updated the code

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Bevásárló kocsi app"
+    return render_template("templates/index.html")
 
 app.run(host="0.0.0.0", port=80)
